869700b40c28c731cbc7a8c1ae17a585a13fa85e
Liam <liam19970731@gmail.com>
2025-07-06 00:20:09 +0800

    first commit

---
+++ src/main/java/com/camping/userservice/ProductController.java
@@ -0,0 +1,13 @@
+package com.camping.userservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @GetMapping("/test")
+    public String getProduct() {
+        System.out.println("getProduct");
+        return "FUCK YOU Terry!";
+    }
+}
